656fe214d4da0f0b35a2ecc2f6ff850662a435f8
Dhiraj Jadhav <dhiraj-jadhav@outlook.com>
2023-01-08 11:09:54 +0530

    file of first code is added

---
+++ src/com/velocity/CollectionDemoGit.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionDemoGit 
+{
+	public static void main(String[] args) 
+	{
+		List<Integer>list=new ArrayList<>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		System.out.println(list);
+		
+		
+	}
+}
