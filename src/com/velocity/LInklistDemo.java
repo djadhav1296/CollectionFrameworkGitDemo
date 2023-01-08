3aca8b179d2f3e2f04329b3c965176be6863e7e5
Dhiraj Jadhav <dhiraj-jadhav@outlook.com>
2023-01-08 11:22:12 +0530

    Second code is Added

---
+++ src/com/velocity/LInklistDemo.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LInklistDemo 
+{
+	public static void main(String[] args) 
+	{
+		List<String>list=new LinkedList<>();
+		list.add("Amol");
+		list.add("AMit");
+		System.out.println(list);
+	}
+}
